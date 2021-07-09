561c7dd93217b27253dabfad1d82d836f8abd7fb
Francisco Casado <fcasado@ug.uchile.cl>
2021-07-09 08:43:02 -0400

    mvp ready

--- group-rand.js
+++ group-rand.js
@@ -21,13 +21,15 @@ document.getElementById('import').onclick = function () {
         clearBox('container')
         result.grupos.forEach(function (element) {
 
-            htmlStructure += "<div id=group-" + element.id + "> " + element.id +"<ul>"
-                
-            element.integrantes.forEach(function (element){
-                htmlStructure += "<li>" + element + "</li>"
+            htmlStructure += "<div class=group id=group-" + element.id + "> " + element.id 
+
+            var person_id = 1;    
+            element.integrantes.forEach(function (person_name){
+                htmlStructure += "<div class=person id=group-" + element.id + "-" + person_id + ">" + person_name + "</div>"
+                person_id++;
             })
 
-            htmlStructure += "</ul></div>"
+            htmlStructure += "</div>"
         });
 
 
@@ -37,7 +39,47 @@ document.getElementById('import').onclick = function () {
     fr.readAsText(files.item(0));
 };
 
+var group_id = "";
+
+document.getElementById('group').onclick = function () {
+    var total_groups = document.getElementById('container').childElementCount;
+    group_id = "group-" + randomInt(1,total_groups);
+    clearGroups();
+    document.getElementById(group_id).style.background = 'green';
+    console.log(group_id)
+
+}
+
+document.getElementById('presenter').onclick = function () {
+    console.log(group_id)
+    var total_people = document.getElementById(group_id).childElementCount;
+    var person_id = group_id + "-" + randomInt(1,total_people);
+    clearPeople();
+    document.getElementById(person_id).style.background = 'yellow';
+
+}
+
+
+
 function clearBox(elementID)
 {
     document.getElementById(elementID).innerHTML = "";
 }
+
+function clearGroups() {
+    var elements = document.getElementsByClassName("group");
+    Array.prototype.forEach.call (elements, function(element) {
+        element.style.background = "none";
+    });
+}
+
+function clearPeople() {
+    var elements = document.getElementsByClassName("person");
+    Array.prototype.forEach.call (elements, function(element) {
+        element.style.background = "none";
+    });
+}
+
+function randomInt (min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+};
