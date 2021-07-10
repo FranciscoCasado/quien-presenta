4d5eea71db920080efdbc2046cb7b0b36645058c
Francisco Casado <fcasado@ug.uchile.cl>
2021-07-09 22:15:23 -0400

    Refactored to comply with JSLint rules

--- app.js
+++ app.js
@@ -1,85 +1,102 @@
-var randomProperty = function (obj) {
-    var keys = Object.keys(obj);
-    return obj[keys[keys.length * Math.random() << 0]];
-};
+/*jslint browser:true */
+/*jslint devel: true */
+/*global $, jQuery, alert, FileReader*/
 
-document.getElementById('import').onclick = function () {
-    var files = document.getElementById('selectFiles').files;
-    console.log(files);
-    if (files.length <= 0) {
-        return false;
-    }
-
-    var fr = new FileReader();
-
-    fr.onload = function (e) {
-        console.log(e);
-        var result = JSON.parse(e.target.result);
-        var formatted = JSON.stringify(result, null, 2);
-
-        var htmlStructure="";
-        clearBox('container');
-        result.grupos.forEach(function (element) {
-
-            htmlStructure += "<div class=group id=group-" + element.id + "> <div class=group-header> #" + element.id + "</div>"
-
-            var person_id = 1;    
-            element.integrantes.forEach(function (person_name){
-                htmlStructure += "<div class=person id=group-" + element.id + "-" + person_id + ">" + person_name + "</div>"
-                person_id++;
-            })
-
-            htmlStructure += "</div>"
-        });
-
-
-        $('#container').append(htmlStructure)
-
-    }
-    fr.readAsText(files.item(0));
-};
-
+/* Global variable for setting group id*/
 var group_id = "";
 
-document.getElementById('group').onclick = function () {
-    var total_groups = document.getElementById('container').childElementCount;
-    group_id = "group-" + randomInt(1,total_groups);
-    clearGroups();
-    clearPeople();
-    document.getElementById(group_id).style.background = '#ccdbfd';
 
+var randomProperty = function (obj) {
+    "use strict";
+    var keys = Object.keys(obj);
+    return obj[keys[Math.floor(keys.length * Math.random())]];
+};
+
+function randomInt(min, max) {
+    "use strict";
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-document.getElementById('presenter').onclick = function () {
-    var total_people = document.getElementById(group_id).childElementCount -1;
-    var person_id = group_id + "-" + randomInt(1,total_people);
-    clearPeople();
-    console.log(person_id)
-    document.getElementById(person_id).style.background = '#ffeedd';
-
-}
-
-
-
-function clearBox(elementID)
-{
+function clearBox(elementID) {
+    "use strict";
     document.getElementById(elementID).innerHTML = "";
 }
 
 function clearGroups() {
+    "use strict";
     var elements = document.getElementsByClassName("group");
-    Array.prototype.forEach.call (elements, function(element) {
+    Array.prototype.forEach.call(elements, function (element) {
         element.style.background = "white";
     });
 }
 
 function clearPeople() {
+    "use strict";
     var elements = document.getElementsByClassName("person");
-    Array.prototype.forEach.call (elements, function(element) {
+    Array.prototype.forEach.call(elements, function (element) {
         element.style.background = "none";
     });
 }
 
-function randomInt (min, max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
+/* Highlight a group element */
+document.getElementById('group').onclick = function () {
+    "use strict";
+    var total_groups = document.getElementById('container').childElementCount;
+
+    /* Remember group_id is global */
+    group_id = "group-" + randomInt(1, total_groups);
+
+    clearGroups();
+    clearPeople();
+
+    document.getElementById(group_id).style.background = '#ccdbfd';
+
+};
+
+/* Highlight a person inside the current highlighted element */
+document.getElementById('presenter').onclick = function () {
+    "use strict";
+    var total_people = document.getElementById(group_id).childElementCount - 1,
+        person_id = group_id + "-" + randomInt(1, total_people);
+
+    clearPeople();
+
+    document.getElementById(person_id).style.background = '#ffeedd';
+};
+
+
+/* Build the HTML elements for groups and people, based on the imported .json file */
+document.getElementById('import').onclick = function () {
+    "use strict";
+    var files = document.getElementById('selectFiles').files,
+        fr = new FileReader();
+
+    console.log(files);
+    if (files.length <= 0) {
+        return false;
+    }
+
+    fr.onload = function (e) {
+        console.log(e);
+        var result = JSON.parse(e.target.result),
+            htmlStructure = "";
+
+        clearBox('container');
+        result.grupos.forEach(function (element) {
+
+            htmlStructure += "<div class=group id=group-" + element.id + "> <div class=group-header> #" + element.id + "</div>";
+
+            var person_id = 1;
+            element.integrantes.forEach(function (person_name) {
+                htmlStructure += "<div class=person id=group-" + element.id + "-" + person_id + ">" + person_name + "</div>";
+                person_id += 1;
+            });
+
+            htmlStructure += "</div>";
+        });
+
+        $('#container').append(htmlStructure);
+
+    };
+    fr.readAsText(files.item(0));
 };
